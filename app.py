83194578f49aeaad8bd6380cda1f0b3c6fb9c51e
prasannalakshmi saili <110597387+prasannalakshmi01@users.noreply.github.com>
2023-02-27 10:34:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.header("Hello :red[everyone],")
+st.subheader("I'am Prasannalakshmi")
+st.subheader("Welcome to my :blue[web application]")
+
+option=st.button("click here to connect")
+if option==True:
+    st.balloons()
+
+    linkedin="https://www.linkedin.com/in/prasannalakshmi-saili-7a0257246/"
+    var1=st.write(format(linkedin))
+    
