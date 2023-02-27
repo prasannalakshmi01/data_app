12ec1708d2d4cd7ca1832aa259e546de76650a9e
prasannalakshmi saili <110597387+prasannalakshmi01@users.noreply.github.com>
2023-02-27 11:35:20 +0530

    Add files via upload

---
+++ pages/about_data.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+
+df=pd.read_csv('resources/company_data.csv')
+
+st.subheader("Worlds Top 500 :red[Companies] by Market Capitalization as on Jan 1st,2020")
+option = st.radio("",('Head','Tail','check null values','shape'))
+
+
+if option == 'Head':
+    st.markdown('**Head of the dataframe**')
+    st.dataframe(df.head())
+elif option == 'Tail':
+    st.markdown('**Tail of the dataframe**')
+    st.dataframe(df.tail())
+elif option == 'shape':
+    st.text('Number of rows: {}'.format(df.shape[0]))
+    st.text('Number of Columns: {}'.format(df.shape[1]))
+elif option == 'check null values':
+    st.markdown('**As we can see that there is no null values in dataset**')
+    st.text(df.isna().sum())
+elif option == 'info':
+    st.markdown('**As we can see that there is no null values in dataset**')
+    st.text(df.info())
+# elif option =='describe':
+st.subheader('Statistical Information')
+
+btn_click = st.button('Describe')
+
+if btn_click == True:
+    st.dataframe(df.describe())
---
+++ pages/dashboard.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from matplotlib import image
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import plotly.express as px
+import os
+
+df=pd.read_csv('resources/company_data.csv')
+
+
+
+st.subheader('Top 10 Companies Based On Market Cap')
+fig1=plt.figure(figsize=(10,5))
+plt.barh(df.Company_Names[0:10],df['Market_Cap(In_Billion_USD)'][0:10],color='red',edgecolor='blue')
+plt.xlabel('Market Cap',fontsize=25)
+plt.ylabel('Company_Names',fontsize=25);
+st.pyplot(fig1)
+
+st.subheader('Count of Headquarter of Company')
+
+fig=plt.figure()
+df['Headquarter_of_Company'].value_counts().plot(kind="bar")
+st.pyplot(fig)
